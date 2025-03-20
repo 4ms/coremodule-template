d3677a38977b2264dc72999c542a38d30cd55147
Dan Green <danngreen1@gmail.com>
2025-03-20 12:53:21 -0700

    Temp remove uppercase file

--- src/vcv/modules/MYMODULE_vcv.cc
+++
@@ -1,7 +0,0 @@
-#include "core/MYMODULE.hh"
-#include "info/MYMODULE_info.hh"
-#include "vcv/generic_module.hh"
-
-using namespace MetaModule;
-
-rack::Model *modelMYMODULE = GenericModule<MYMODULEInfo, MYMODULECore>::create();
